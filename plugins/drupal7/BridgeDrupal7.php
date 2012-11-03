3737af402b845edd8b691375bccf966e6795766a
Christoph Burschka <christoph@burschka.de>
2012-11-03 02:05:17 +0100

    New Drupal 7 plugin.

---
+++ plugins/drupal7/BridgeDrupal7.php
@@ -0,0 +1,23 @@
+<?php
+
+class BridgeDrupal7 extends EjabberdAuthBridge {
+  function isuser($username, $server) {
+    return user_load_by_name($username) !== FALSE;
+  }
+
+  function auth($username, $server, $password) {
+    return user_authenticate($username, $password) !== FALSE;
+  }
+
+  function setpass($username, $server, $password) {
+    return FALSE;
+  }
+
+  function tryregister($username, $server, $password) {
+    return FALSE;
+  }
+
+  function removeuser($username, $server) {
+    return FALSE;
+  }
+}
