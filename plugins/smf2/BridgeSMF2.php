6dc4cdc44ebc6e47fba1447a7a66adc7468ff154
Christoph Burschka <christoph@burschka.de>
2013-12-30 03:08:18 +0100

    SMF's SSI API may randomly spew HTML; run it in an output buffer.

--- plugins/smf2/BridgeSMF2.php
+++ plugins/smf2/BridgeSMF2.php
@@ -6,16 +6,15 @@
 class BridgeSMF2 extends EjabberdAuthBridge {
   function isuser($username, $server) {
     $query_where = 'member_name = {string:name}';
-
     $query_where_params = array(
-      'name' => $name,
+      'name' => $username,
     );
 
-    return 1 == count(ssi_queryMembers($query_where, $query_where_params, 1, 'id_member DESC', NULL));
+    return 1 == count(smf_ssi('queryMembers', $query_where, $query_where_params, 1, 'id_member DESC', NULL));
   }
 
   function auth($username, $server, $password) {
-    return ssi_checkPassword($username, $password, TRUE);
+    return smf_ssi('checkPassword', $username, $password, TRUE);
   }
 
   function setpass($username, $server, $password) {
